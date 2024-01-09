a240be54498953f86295ed9789dd5dc9cae7ac38
Neelimarani-git <128829175+Neelimarani-git@users.noreply.github.com>
2024-01-09 18:06:51 +0100

    QT_Concurrent
    QT_Concurrent

--- main.cpp
+++ main.cpp
@@ -1,39 +1,32 @@
+// very easy, removes the complexity of threading
+
 #include <QCoreApplication>
-#include<QSemaphore>
-#include<QList>
-#include<QDebug>
-#include<QThread>
-#include<QRandomGenerator>
-#include "worker.h"
+#include <QtConcurrent>
+
+int do_map(int value)
+{
+    qInfo() <<"Do stuff: " << value;
+    int num = value * 10;
+    return num;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     QList<int> values;
-    values.resize(10);
-    values.fill(0);
 
-    QSemaphore sema(values.size());
-    qInfo() << values;
+    for(int i=0; i < 9; i++)
+    {
+        values << i;
+    }
 
-    QThread thread1;
-    QThread thread2;
+    qInfo() << "Starting..";
+    QList<int> updated = QtConcurrent::blockingMapped(values, &do_map);
 
-    thread1.setObjectName("Thread 1");
-    thread2.setObjectName("Thread 2");
+    qInfo() << "Finished";
 
-
-    Worker worker1(&sema,&values);
-    Worker worker2(&sema,&values);
-    worker1.moveToThread(&thread1);
-    worker2.moveToThread(&thread2);
-
-    QObject::connect(&thread1,&QThread::started,&worker1,&Worker::run);
-    QObject::connect(&thread2,&QThread::started,&worker2,&Worker::run);
-
-    thread1.start();
-    thread2.start();
+    qInfo() << updated;
 
     return a.exec();
 }
